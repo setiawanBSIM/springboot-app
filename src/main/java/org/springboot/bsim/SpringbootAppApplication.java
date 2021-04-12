ee5fa78349c372a809db4a820041fe8ffec2ad53
setiawanBSIM <setiawan.handoko@banksinarmas.com>
2021-04-12 14:47:36 +0700

    inisialisasi_springboot_apps

---
+++ src/main/java/org/springboot/bsim/SpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.bsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springboot/bsim/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.bsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
